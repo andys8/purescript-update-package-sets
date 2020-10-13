3606bc8b955b4f2bdcd75105d82387f288c43150
andys8 <andys8@users.noreply.github.com>
2020-10-13 12:40:41 +0200

    Update existing github issue

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,7 @@ import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
-import Github (GithubToken(..))
+import Github (GithubToken(..), requestPatchIssue)
 import Node.Process (argv, exit)
 import PackageSets (requestPackages)
 import VersionCompare (VersionComparison(..), comparePackage)
@@ -22,10 +22,14 @@ main = do
 run :: GithubToken -> Aff Unit
 run token = do
   packages <- requestPackages
-  for_ packages
-    $ \package -> do
-        comparison <- comparePackage token package
-        case comparison of
-          VersionOkay _ -> pure unit
-          _ -> log $ show package <> " " <> show comparison
+  -- for_ packages
+  --   $ \package -> do
+  --       comparison <- comparePackage token package
+  --       case comparison of
+  --         VersionOkay _ -> pure unit
+  --         _ -> log $ show package <> " " <> show comparison
+  requestPatchIssue token
+    { title: "Test creating an issue with api"
+    , body: "TestBody\nContent"
+    }
   liftEffect $ exit 0
