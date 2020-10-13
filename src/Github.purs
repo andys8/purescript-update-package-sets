38fcaebb68f3fd1c465e51a6c7af735032a53751
andys8 <andys8@users.noreply.github.com>
2020-10-13 12:40:41 +0200

    Update existing github issue

--- src/Github.purs
+++ src/Github.purs
@@ -4,9 +4,13 @@ import Prelude
 import Affjax (URL)
 import Affjax.RequestHeader (RequestHeader(..))
 import Data.Either (Either, either)
+import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Partial.Unsafe (unsafeCrashWith)
-import Simple.Ajax (AjaxError, getR)
+import Simple.Ajax (AjaxError, HTTPError, getR, patchR_)
+
+newtype GithubToken
+  = GithubToken String
 
 type Tag
   = { name :: String }
@@ -19,19 +23,48 @@ type Repository
     , repoName :: String
     }
 
-newtype GithubToken
-  = GithubToken String
+type IssueContent
+  = { title :: String
+    , body :: String
+    }
+
+baseURL :: URL
+baseURL = "https://api.github.com"
 
 requestTags :: GithubToken -> Repository -> Aff (Array TagName)
-requestTags (GithubToken token) repository = do
-  resp <- getR { headers } $ tagUrlApiUrl repository :: Aff (Either AjaxError (Array Tag))
+requestTags token repository = do
+  resp <-
+    getR
+      { headers: [ apiHeaders token ] }
+      (url repository) ::
+      Aff (Either AjaxError (Array Tag))
   pure $ either (unsafeCrashWith <<< show) ((<$>) _.name) resp
   where
-  headers = [ RequestHeader "Authorization" ("Bearer " <> token) ]
+  url { repoUser, repoName } =
+    baseURL
+      <> "/repos/"
+      <> repoUser
+      <> "/"
+      <> repoName
+      <> "/tags"
 
-tagUrlApiUrl :: Repository -> URL
-tagUrlApiUrl { repoUser, repoName } = baseURL <> path
+apiHeaders :: GithubToken -> RequestHeader
+apiHeaders (GithubToken token) = RequestHeader "Authorization" ("Bearer " <> token)
+
+requestPatchIssue :: GithubToken -> IssueContent -> Aff Unit
+requestPatchIssue token { title, body } = do
+  resp <-
+    patchR_
+      { headers: [ apiHeaders token ] }
+      url
+      (Just $ { state: "open", title, body }) ::
+      Aff (Either HTTPError Unit)
+  pure $ either (unsafeCrashWith <<< show) (identity) resp
   where
-  baseURL = "https://api.github.com"
+  user = "andys8"
 
-  path = "/repos/" <> repoUser <> "/" <> repoName <> "/tags"
+  repo = "package-sets"
+
+  issue = "1"
+
+  url = baseURL <> "/repos/" <> user <> "/" <> repo <> "/issues/" <> issue
